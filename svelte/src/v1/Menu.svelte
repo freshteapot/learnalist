f5fe6b62a809a9aea728d183acbaeddb7b2ed015
Christopher Williams <christopher.williams@amedia.no>
2019-09-09 09:07:52 +0200

    fixup

--- svelte/src/v1/Menu.svelte
+++ svelte/src/v1/Menu.svelte
@@ -1,14 +1,14 @@
 <svelte:options tag="v1-menu"/>
 <script>
-	export let playScreen = "#play"
-	export let infoScreen = "#list-info"
-	console.log(playScreen)
-	function showSlideshow(event) {
-		console.log(playScreen)
-		console.log(document.querySelector(playScreen))
-		document.querySelector(playScreen).style.display = "";
-		document.querySelector(infoScreen).style.display = "none"
-	}
+	export let slideshow = "1";
+	export let playScreen = "#play";
+	export let infoScreen = "#list-info";
 
+	function showSlideshow(event) {
+		document.querySelector(playScreen).style.display = "";
+		document.querySelector(infoScreen).style.display = "none";
+	}
 </script>
+{#if slideshow == "1"}
 <button on:click={showSlideshow}>Slideshow</button>
+{/if}
