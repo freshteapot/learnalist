e378058565b9ddca554c8d8d7e2eaa4afb038742
Christopher Williams <christopher.williams@amedia.no>
2019-09-12 19:15:15 +0200

    working

--- svelte/src/user.js
+++ svelte/src/user.js
@@ -2,4 +2,3 @@ import Login from './user/login.svelte';
 import Logout from './user/logout.svelte';
 import ForgotPassword from './user/forgot-password.svelte';
 import Register from './user/register.svelte';
-import version from './version.json';
