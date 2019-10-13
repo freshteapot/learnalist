2ec8ffa372150685a1044ce50e1863ced1be9891
Christopher Williams <christopher.williams@amedia.no>
2019-10-13 17:41:14 +0200

    fixup

--- server/api/api/version_test.go
+++ server/api/api/version_test.go
@@ -1,30 +1,5 @@
 package api_test
 
-/*
-func (suite *ApiSuite) TestGetVersion() {
-	var raw map[string]interface{}
-	statusCode, responseBytes := suite.getVersion()
-	suite.Equal(http.StatusOK, statusCode)
-	json.Unmarshal(responseBytes, &raw)
-	suite.Equal("n/a", raw["gitHash"].(string))
-	suite.Equal("n/a", raw["gitDate"].(string))
-	suite.Equal("n/a", raw["version"].(string))
-	suite.Equal("https://github.com/freshteapot/learnalist-api/commit/n_a", raw["url"].(string))
-}
-
-func (suite *ApiSuite) getVersion() (statusCode int, responseBytes []byte) {
-	method := http.MethodGet
-	uri := "/api/v1/version"
-
-	req, rec := setupFakeEndpoint(method, uri, "")
-	e := echo.New()
-	c := e.NewContext(req, rec)
-	suite.NoError(m.V1GetVersion(c))
-	return rec.Code, rec.Body.Bytes()
-}
-*/
-
-//2, as I am being really lazy :(, once all moved over to ginkgo remove.
 import (
 	"encoding/json"
 	"net/http"
