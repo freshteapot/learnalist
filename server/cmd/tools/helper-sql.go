750707f649d2fb6f18ff4e88ec2eaabeb364ec0f
Christopher Williams <chris@freshteapot.net>
2019-11-12 19:48:50 +0100

    fixup: making the stub-sql-file command a little better

--- server/cmd/tools/helper-sql.go
+++ server/cmd/tools/helper-sql.go
@@ -10,8 +10,10 @@ import (
 var stubSQLFileCmd = &cobra.Command{
 	Use:   "stub-sql-file",
 	Short: "Output the name of an sql file",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		prefix := fmt.Sprintf("%s-XXX.sql", time.Now().UTC().Format("200601021504"))
-		fmt.Println(prefix)
+		name := args[0]
+		path := fmt.Sprintf("db/%s-%s.sql", time.Now().UTC().Format("200601021504"), name)
+		fmt.Println(path)
 	},
 }
