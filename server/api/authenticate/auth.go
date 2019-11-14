90562bee7df33fdd10e70b8882997e44288745f1
Christopher Williams <chris@freshteapot.net>
2019-11-14 04:17:17 +0100

    tidying up the code a little

--- server/api/authenticate/auth.go
+++ server/api/authenticate/auth.go
@@ -1,10 +1,11 @@
 package authenticate
 
 import (
+	"encoding/base64"
 	"fmt"
 	"hash/fnv"
+	"net/http"
 	"strings"
-	"encoding/base64"
 
 	"github.com/freshteapot/learnalist-api/server/api/uuid"
 	"github.com/labstack/echo/v4"
@@ -18,49 +19,46 @@ var LookupBasic func(loginUser LoginUser) (*uuid.User, error)
 var LookupBearer func(token string) (*uuid.User, error)
 
 func Auth(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			var valid bool
-			var err error
+	return func(c echo.Context) error {
+		var valid bool
+		var err error
 
-			if skip(c) {
-				return next(c)
-			}
-
-			authorization := c.Request().Header.Get("Authorization")
-			parts := strings.SplitN(authorization, " ", 2)
-			fmt.Println(parts)
-			if len(parts) != 2 {
-				return echo.ErrForbidden
-			}
-
-			what := strings.ToLower(parts[0])
-
-			switch what {
-			case "basic":
-				fmt.Println("basic")
-				hash := parts[1]
-				valid, err = validateBasic(c, hash)
-			case "bearer":
-				fmt.Println("bearer")
-				token := parts[1]
-				valid, err = validateBearer(c, token)
-			default:
-				return echo.ErrForbidden
-			}
-
-			if err != nil {
-				return echo.ErrInternalServerError
-			}
-
-			if valid {
-				return next(c)
-			}
-
-			realm := defaultRealm
-			// Need to return `401` for browsers to pop-up login box.
-			c.Response().Header().Set(echo.HeaderWWWAuthenticate, what+" realm="+realm)
-			return echo.ErrUnauthorized
+		if skip(c) {
+			return next(c)
 		}
+
+		authorization := c.Request().Header.Get("Authorization")
+		parts := strings.SplitN(authorization, " ", 2)
+		if len(parts) != 2 {
+			return echo.ErrForbidden
+		}
+
+		what := strings.ToLower(parts[0])
+
+		switch what {
+		case "basic":
+			hash := parts[1]
+			valid, err = validateBasic(c, hash)
+		case "bearer":
+			token := parts[1]
+			valid, err = validateBearer(c, token)
+		default:
+			return echo.ErrForbidden
+		}
+
+		if err != nil {
+			return echo.ErrInternalServerError
+		}
+
+		if valid {
+			return next(c)
+		}
+
+		realm := defaultRealm
+		// Need to return `401` for browsers to pop-up login box.
+		c.Response().Header().Set(echo.HeaderWWWAuthenticate, what+" realm="+realm)
+		return echo.ErrUnauthorized
+	}
 
 }
 
@@ -68,29 +66,31 @@ func skip(c echo.Context) bool {
 	url := c.Request().URL.Path
 	method := c.Request().Method
 	url = strings.TrimPrefix(url, "/api/v1")
-	if url == "/" {
-		return true
-	}
 
-	if url == "/version" && method == "GET" {
-		return true
-	}
-
-	if strings.HasPrefix(url, "/oauth/") {
-		return true
-	}
-
-	// TODO this is shit, should figure out how I want to do this in the future.
-	// Allow some more
-	if strings.HasPrefix(c.Request().RemoteAddr, "127.0.0.1:") {
-		method := c.Request().Method
-		if url == "/register" && method == "POST" {
+	switch method {
+	case http.MethodGet:
+		if url == "/" {
 			return true
 		}
+
+		if url == "/version" {
+			return true
+		}
+
+		if strings.HasPrefix(url, "/oauth/") {
+			return true
+		}
+	case http.MethodPost:
+		// Unfiltered ability to register a user
+		if url == "/register" {
+			return true
+		}
+	default:
+		return false
 	}
+
 	return false
 }
-
 
 func validateBearer(c echo.Context, token string) (bool, error) {
 	user, err := LookupBearer(token)
