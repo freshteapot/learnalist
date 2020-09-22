20ae5fb91ef089320479d6049bed4020f9a43384
Christopher Williams <chris@freshteapot.net>
2020-09-22 19:19:36 +0200

    fixup

--- server/pkg/acl/keys/keys.go
+++ server/pkg/acl/keys/keys.go
@@ -7,13 +7,6 @@ const (
 	KindSharePublic        = "api:%s:%s:share:public"  // kind, extUUID
 	KindSharePrivate       = "api:%s:%s:share:private" // kind, extUUID
 	KindShareFriends       = "api:%s:%s:share:friends" // kind, extUUID
-
-	ListWriteAccessForUser = "api:list:%s:write:%s"
-	ListReadAccessForUser  = "api:list:%s:read:%s"
-	ListOwnerAccessForUser = "api:list:%s:owner:%s"
-	ListSharePublic        = "api:list:%s:share:public"
-	ListSharePrivate       = "api:list:%s:share:private"
-	ListShareFriends       = "api:list:%s:share:friends"
 	FilterListShare        = "api:list:%s:share:%%"
 	SharedWithPublic       = "public"
 	NotShared              = "private"
