cb20b72b81b3b959f0cd62e02e49c9a5b4b9d6fe
freshteapot <chris@freshteapot.net>
2021-03-19 00:43:56 +0100

    fixup

--- js/rollup.config.browser-extension.import-play.js
+++ js/rollup.config.browser-extension.import-play.js
@@ -2,36 +2,42 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import sveltePreprocess from 'svelte-preprocess';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
 	input: 'src/browser-extension/import-play/main.js',
 	output: {
-
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
 		file: 'browser-extension/import-play/bundle.js'
 	},
 	plugins: [
-
 		typescript(),
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('browser-extension/import-play/bundle.css');
+			onwarn: (warning, handler) => {
+				const { code, frame } = warning;
+				if (code === "css-unused-selector")
+					return;
+
+				handler(warning);
+			},
+			preprocess: sveltePreprocess({
+				postcss: {
+					configFilePath: "./postcss.config.js",
+				},
+			}),
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
+				customElement: false
 			},
 		}),
+		css({ output: 'bundle.css' }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
 			dedupe: ['svelte']
