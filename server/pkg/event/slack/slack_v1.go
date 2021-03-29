260fb4fc60764f1f9ef5d107a5af034389080270
freshteapot <chris@freshteapot.net>
2021-03-29 15:49:00 +0200

    fixup

--- server/pkg/event/slack/slack_v1.go
+++ server/pkg/event/slack/slack_v1.go
@@ -250,10 +250,6 @@ func (s SlackEvents) Read(entry event.Eventlog) {
 			msg.Text = fmt.Sprintf(`%s action not supported %s`, entry.Kind, entry.Action)
 		}
 	case event.ApiAppSettingsRemindV1:
-		//b, _ := json.Marshal(entry.Data)
-		//var settings openapi.AppSettingsRemindV1
-		//json.Unmarshal(b, &settings)
-
 		userUUID := entry.UUID
 		switch entry.Action {
 		case event.ActionUpsert:
