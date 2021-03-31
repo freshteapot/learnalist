da7fe2a15123a561d66b8d87a3c7105162d04bce
freshteapot <chris@freshteapot.net>
2021-03-31 17:04:02 +0200

    My commands were too fancy

--- scripts/build-browser-extension-chrome-menu-add-spaced-repetition.sh
+++ scripts/build-browser-extension-chrome-menu-add-spaced-repetition.sh
@@ -3,10 +3,17 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_PWD="$CWD/.."
 
 cd "$ROOT_PWD"
+rm -rf /tmp/learnalist/browser-extension
 mkdir -p /tmp/learnalist/browser-extension
 cp -r js/browser-extension/menu-add-spaced-repetition /tmp/learnalist/browser-extension
 cd /tmp/learnalist/browser-extension/menu-add-spaced-repetition
-zip -q ../menu-add-spaced-repetition.zip ./*
+zip -rq ../menu-add-spaced-repetition.zip ./*
+cd /tmp/learnalist/browser-extension/
+mkdir temp
+cp menu-add-spaced-repetition.zip temp
+cd temp
+unzip menu-add-spaced-repetition.zip
+
 
 cat  << _EOF_
 Next:
